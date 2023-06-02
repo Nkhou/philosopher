003a428280604703e86819c59df3fbeff5ea6248
Najat Khoudrouf <nkhoudro@e2r12p1.1337.ma>
2023-06-02 21:23:19 +0100

    philo mod

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 /*   By: nkhoudro <nkhoudro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:33:28 by nkhoudro          #+#    #+#             */
-/*   Updated: 2023/06/01 21:29:05 by nkhoudro         ###   ########.fr       */
+/*   Updated: 2023/06/02 21:22:46 by nkhoudro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void	*retune()
+void	*retune(void *tred)
 {
-	// pthread_mutex_lock(&tread.mutex);
-	printf("hello everyone\n");
-	printf("hello goodbye\n");
+	t_data	*tread;
+
+	tread = (t_data *) tred;
+	pthread_mutex_lock(&tread->forks);
+	printf("tread %d has taken a fork \n", tread->index);
+	printf("tread %d is is eating \n", tread->index);
+	pthread_mutex_unlock(&tread->forks);
 	return (0);
 }
 
@@ -39,7 +43,7 @@ void	check_arguments(char **argv)
 	}
 }
 
-void	ft_initial(char **argv, int ac, t_philosopher *tread)
+void	ft_initial(char **argv, int ac, t_data *tread)
 {
 	int	i;
 
@@ -52,32 +56,50 @@ void	ft_initial(char **argv, int ac, t_philosopher *tread)
 	tread->time_to_die = ft_atoi(argv[2]);
 	tread->time_to_eat = ft_atoi(argv[3]);
 	tread->time_to_sleep = ft_atoi(argv[4]);
+	tread->index = 0;
 }
 
-void	ft_creat_fork(t_philosopher *tread)
+void	ft_creat_fork(t_data *tread)
 {
 	int i;
 
 	i = 0;
 	while (i < tread->num_fork)
 	{
-		if (pthread_create(&tread->tread[i], NULL, &retune, NULL) != 0)
+		tread->tread[i] = malloc(sizeof(pthread_t));
+		if (pthread_create(&tread->tread[i], NULL, &retune, tread) != 0)
 			ft_error("Error\n");
 		if (pthread_join(tread->tread[i], NULL) != 0)
 			ft_error("Error\n");
 		i++;
+		tread->index = i;
 	}
+	// i = 0;
+	// while (i < tread->num_fork)
+	// {
+	// 	i++;
+	// }
+	// // i = 0;
+	// while (i < tread->num_fork)
+	// {
+	// 	if (i % 2 != 0)
+	// 	{
+	// 		if (pthread_join(tread->tread[i], NULL) != 0)
+	// 			ft_error("Error\n");
+	// 	}
+	// 	i++;
+	// }
 }
 int	main(int ac, char **argv)
 {
-	t_philosopher	tread;
+	t_data	tread;
 
 	if (ac != 5 && ac != 6)
 		ft_error("Error in number of arguments\n");
-	pthread_mutex_init(&(tread.mutex), NULL);
 	check_arguments(argv);
 	ft_initial(argv, ac, &tread);
+	pthread_mutex_init(&(tread.forks), NULL);
 	ft_creat_fork(&tread);
-	pthread_mutex_destroy(&tread.mutex);
+	pthread_mutex_destroy(&tread.forks);
 	return (0);
 }
--- philosopher.h
+++ philosopher.h
@@ -6,7 +6,7 @@
 /*   By: nkhoudro <nkhoudro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:35:16 by nkhoudro          #+#    #+#             */
-/*   Updated: 2023/05/30 11:52:05 by nkhoudro         ###   ########.fr       */
+/*   Updated: 2023/06/02 21:19:30 by nkhoudro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <pthread.h>
 # include <stdio.h>
 
-typedef struct s_philosopher
+typedef struct s_data
 {
 	pthread_t	*tread;
 	int			num_philo;
@@ -25,10 +25,23 @@ typedef struct s_philosopher
 	int			time_to_die;
 	int			time_to_eat;
 	int			time_to_sleep;
+	int			index;
 	int			num_time_to_eat;
-	pthread_mutex_t mutex;
-}	t_philosopher;
+	pthread_mutex_t forks;
+	pthread_mutex_t lock;
+	pthread_mutex_t write;
+}	t_data;
 
+typedef struct s_philosopher
+{
+	// struct *data;
+	pthread_t tmp;
+	int id;
+	int			num_time_to_eat;
+	int			num_time_was_eat;
+	int			time_to_die;
+	
+} t_philosopher;
 void	ft_error(char *str);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
